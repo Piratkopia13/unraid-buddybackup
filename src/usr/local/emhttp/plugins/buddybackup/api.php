b9d10bac712f7dfefd8ac10912b38e1595aa0b6a
Alexander Wester <alexander.wester@gmail.com>
2025-01-23 19:18:19 +0100

    [wip] Add Restore.page section
    Add api function to fetch snapshot list from buddy

--- src/usr/local/emhttp/plugins/buddybackup/api.php
+++ src/usr/local/emhttp/plugins/buddybackup/api.php
@@ -1,7 +1,23 @@
 <?php
     $plugin = "buddybackup";
+    
+    function get_buddy_snapshots() {
+        if ($json = shell_exec("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup get_buddy_snapshots")) {
+            echo $json;
+        } else {
+            echo '{"status": "failed", "error": "Failed to retrieve buddy\'s snapshots"}';
+        }
+    }
 
-    if ($_POST["cmd"] == "get_log") {
-        echo file_get_contents("/var/log/buddybackup.log");
+    switch ($_POST["cmd"]) {
+        case 'get_log':
+            echo file_get_contents("/var/log/buddybackup.log");
+            break;
+        case 'get_buddy_snapshots':
+            get_buddy_snapshots();
+            break;
+        
+        default:
+            break;
     }
 ?>
