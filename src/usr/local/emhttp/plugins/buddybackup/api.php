e5cb94ac2ec63aa88bfb3f56fd7fbb9530717fbb
Alexander Wester <alexander.wester@gmail.com>
2025-02-04 21:04:42 +0100

    Started work on supporting multiple backup sources and destinations

--- src/usr/local/emhttp/plugins/buddybackup/api.php
+++ src/usr/local/emhttp/plugins/buddybackup/api.php
@@ -11,21 +11,29 @@
         }
     }
 
-    function remove_sanoid_section($section) {
-        global $snapshot_cfg;
-        global $snapshot_cfg_file;
-
-        unset($snapshot_cfg[$section]);
+    function remove_ini_section($current_content, $section_to_remove, $file) {
+        unset($current_content[$section_to_remove]);
         $new_content = '';
-        foreach ($snapshot_cfg as $section => $section_content) {
+        foreach ($current_content as $section => $section_content) {
             $section_content = array_map(function($value, $key) {
                 return "$key=$value";
             }, array_values($section_content), array_keys($section_content));
             $section_content = implode("\n", $section_content);
             $new_content .= "[$section]\n$section_content\n";
         }
-        file_put_contents($snapshot_cfg_file, $new_content);
+        file_put_contents($file, $new_content);
+    }
+    function remove_sanoid_section($section) {
+        global $snapshot_cfg;
+        global $snapshot_cfg_file;
+        remove_ini_section($snapshot_cfg, $section, $snapshot_cfg_file);
         echo shell_exec("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup update_sanoid_conf");
+    }
+    function remove_backup_section($section) {
+        global $backup_cfg;
+        global $backup_cfg_file;
+        remove_ini_section($backup_cfg, $section, $backup_cfg_file);
+        echo shell_exec("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup update_backup_conf");
     }
 
     switch ($_POST["cmd"]) {
@@ -37,6 +45,9 @@
             break;
         case 'remove_sanoid_section':
             remove_sanoid_section($_POST["section"]);
+            break;
+        case 'remove_backup_section':
+            remove_backup_section($_POST["section"]);
             break;
         
         default:
--- src/usr/local/emhttp/plugins/buddybackup/common.php
+++ src/usr/local/emhttp/plugins/buddybackup/common.php
@@ -7,8 +7,44 @@
     $rc_name = "rc.{$plugin}";
     $rc_script = "/plugins/{$plugin}/scripts/{$rc_name}";
 
+    $plugin_cfg_file = "/boot/config/plugins/$plugin/$plugin.cfg";
     $snapshot_cfg_file = "/boot/config/plugins/$plugin/snapshots.cfg";
+    $backup_cfg_file = "/boot/config/plugins/$plugin/backups.cfg";
+
+    function overwrite_ini($config, $file) {
+        $content = "";
+        foreach ($config as $key => $value) {
+            $content .= "$key=\"$value\"\n";
+        }
+        return file_put_contents($file, $content) !== false;
+    }
+    function add_to_ini($config, $section, $file) {
+        $content = "[$section]\n";
+        foreach ($config as $key => $value) {
+            $content .= "$key=\"$value\"\n";
+        }
+        // Append content to file
+        return file_put_contents($file, $content, FILE_APPEND) !== false;
+    }
+
+    // backup cfg used to be a single one and live in $cfg. Move it over to $snapshot_cfg if it still exists
+    // LEGACY - to be removed
+    if ($cfg["BackupToBuddy"]) {
+        $backup = array();
+        $backup["enable"] = ($cfg["BackupToBuddy"] == "enable") ? "yes" : "no"; unset($cfg["BackupToBuddy"]);
+        $backup["destination_host"] = $cfg["DestinationHost"]; unset($cfg["DestinationHost"]);
+        $backup["source_dataset"] = $cfg["SourceDataset"]; unset($cfg["SourceDataset"]);
+        $backup["destination_dataset"] = $cfg["SendDestinationDataset"]; unset($cfg["SendDestinationDataset"]);
+        $backup["backup_cron"] = $cfg["BackupCron"]; unset($cfg["BackupCron"]);
+        $backup["recursive"] = $cfg["BackupRecursive"]; unset($cfg["BackupRecursive"]);
+
+        // Update ini files
+        add_to_ini($backup, "1ml3g4cy", $backup_cfg_file);
+        overwrite_ini($cfg, $plugin_cfg_file);
+    }
+
     $snapshot_cfg = my_parse_ini_file($snapshot_cfg_file, true);
+    $backup_cfg = my_parse_ini_file($backup_cfg_file, true);
 
     function datasets($selected, $only_encrypted = true) {
         $datasets = mk_option($selected, "", "Select from list", "disabled");
@@ -28,3 +64,12 @@
         return $datasets;
     }
 ?>
+
+<!-- ReceiveBackups="enable" -->
+<!-- ReceiveDestinationRententionHourly="0" -->
+<!-- ReceiveDestinationRententionDaily="7" -->
+<!-- ReceiveDestinationRententionWeekly="4" -->
+<!-- ReceiveDestinationRententionMonthly="3" -->
+<!-- ReceiveDestinationRententionYearly="0" -->
+<!-- DestinationPubSSHKey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRHUnG8/Qp4b0zJiWjR1tezUkDVLzsc21EUguCcEQRk root@Prism " -->
+<!-- ReceiveDestinationDataset="disk1/prism_offsite_backup" -->
