127e0e6ebd3d503bf2f0a5176fdd289cd63459b3
Alexander Wester <alexander.wester@gmail.com>
2025-02-16 16:27:23 +0100

    Better makepkg.sh that sets file permissions and owner

--- makepkg.sh
+++ makepkg.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-cd src
-makepkg -l y -c y ../buddybackup.txz
-cd ..
-if [ $? -ne 0 ]; then
+SRC_DIR="/mnt/user/sonic/buddybackup_plugin/src"
+TMP_DIR="/tmp/buddybackup_${RANDOM}"
+
+cp -r "${SRC_DIR}" "${TMP_DIR}"
+cd "${TMP_DIR}"
+chown -R root:root ./
+chmod -R 755 ./
+
+rm "${SRC_DIR}/../buddybackup.txz"
+makepkg -l y -c y "${SRC_DIR}/../buddybackup.txz"
+ec=$?
+cd "${SRC_DIR}"
+echo "Removing ${TMP_DIR}"
+rm -r "${TMP_DIR}"
+if [[ "${ec}" -ne 0 ]]; then
     exit "Failed to makepkg, aborting."
 fi
 
 # Update md5 in plg file
-md5=$(md5sum buddybackup.txz | sed 's/\s.*$//')
-sed -i "/<!ENTITY pkgMD5/c\  <!ENTITY pkgMD5        \"${md5}\">" buddybackup.plg
+md5=$(md5sum "${SRC_DIR}/../buddybackup.txz" | sed 's/\s.*$//')
+sed -i "/<!ENTITY pkgMD5/c\  <!ENTITY pkgMD5        \"${md5}\">" "${SRC_DIR}/../buddybackup.plg"
+
+if [[ "${1}" == "reinstall" ]]; then
+    echo "reinstalling.."
+    upgradepkg --install-new --reinstall "${SRC_DIR}/../buddybackup.txz"
+    shift
+fi
+if [[ "${1}" == "update" ]]; then
+    echo "calling rc.buddybackup.php update"
+    /usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup.php update
+fi
