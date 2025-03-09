d0862d1f7d9218fb4ef6e3815aaa8b93330d8388
Alexander Wester <alexander.wester@gmail.com>
2025-03-09 18:32:44 +0100

    Allow aborting all running backups or restores from gui

--- src/usr/local/emhttp/plugins/buddybackup/api.php
+++ src/usr/local/emhttp/plugins/buddybackup/api.php
@@ -28,6 +28,46 @@
         echo shell_exec("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup.php update");
     }
 
+    function get_top_level_pids($filter) {
+        $ret = array();
+        $output = array();
+        exec('pgrep -f "'.$filter.'"', $output);
+        foreach ($output as $pid) {
+            $ppid = shell_exec("ps -o ppid= ".$pid);
+            if ($ppid == "1") {
+                array_push($ret, $pid);
+            }
+        }
+        return $ret;
+    }
+    function get_running_backups_pids() {
+        return get_top_level_pids("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup send*_backup");
+    }
+    function get_running_restore_pids() {
+        return get_top_level_pids("/usr/local/emhttp/plugins/buddybackup/scripts/rc.buddybackup restore_snapshot");
+    }
+
+    function kill_running_backups() {
+        $pids = get_running_backups_pids();
+        foreach ($pids as $pid) {
+            exec("kill ".$pid);
+        }
+        exec("echo 'User aborted ".count($pids)." running backup operation(s)' | /usr/local/emhttp/plugins/buddybackup/scripts/log.sh");
+    }
+    function kill_running_restores() {
+        $pids = get_running_restore_pids();
+        foreach ($pids as $pid) {
+            exec("kill ".$pid);
+        }
+        exec("echo 'User aborted ".count($pids)." running restore operation(s)' | /usr/local/emhttp/plugins/buddybackup/scripts/log.sh");
+    }
+    function count_running_backups() {
+        echo count(get_running_backups_pids());
+    }
+    function count_running_restores() {
+        echo count(get_running_restore_pids());
+    }
+
     switch ($_POST["cmd"]) {
         case 'get_log':
             echo file_get_contents("/var/log/buddybackup.log");
@@ -37,6 +77,18 @@
             break;
         case 'remove_backup_section':
             remove_backup_section($_POST["section"]);
+            break;
+        case 'kill_running_backups':
+            kill_running_backups();
+            break;
+        case 'kill_running_restores':
+            kill_running_restores();
+            break;
+        case 'count_running_backups':
+            count_running_backups();
+            break;
+        case 'count_running_restores':
+            count_running_restores();
             break;
         
         default:
